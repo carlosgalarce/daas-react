d25991bc806f5d1c340dff9b53f390d25d142c56
shoaib <shoaibsilat9@gmail.com>
2020-05-21 03:49:33 +0500

    vehicle specs card rendered

--- src/views/AppPages/ScheduleService.js
+++ src/views/AppPages/ScheduleService.js
@@ -56,6 +56,11 @@ function ScheduleService() {
   const isProgressProviders = useSelector(store => store?.schedule?.isProgressProviders);
   const isProgressInfo = useSelector(store => store?.settings?.isProgressInfo);
   const isProgressBookAppointment = useSelector(store => store?.schedule?.isProgressBookAppointment);
+  const isProgressSpecs = useSelector(store => store?.vehicle?.isProgressSpecs);
+  const isProgressPrice = useSelector(store => store?.vehicle?.isProgressPrice);
+  const vehicleSpecs = useSelector(store => store?.vehicle?.specs);
+  const vehiclePrice = useSelector(store => store?.vehicle?.price);
+  const isProgressRecomndServices = useSelector(store => store?.schedule?.isProgressRecomndServices);
   const vehicles = useSelector(store => store?.settings?.customerInfo?.Vehicles);
   const user = useSelector(store => store?.auth?.user);
   const appointment = useSelector(store => store?.schedule?.appointment);
@@ -152,7 +157,7 @@ function ScheduleService() {
               </CardHeader>
               <CardBody>
                 {
-                  isProgressServices || isProgressProviders || isProgressInfo
+                  isProgressServices || isProgressProviders || isProgressInfo || isProgressPrice || isProgressSpecs || isProgressRecomndServices
                     ?
                     <div className="d-flex justify-content-center align-items-center" >
                       <div className="spinner-grow" ></div>
@@ -273,7 +278,78 @@ function ScheduleService() {
                             </Col>
                           </Row>
                         </Col>
-                        <Col md={'6'} >
+                        <Col md={'6'} className='vehicle-info-container shadow-lg rounded' >
+                          <h1 className="text-center" >Vehicle Info</h1>
+                          <h2 className="text-primary" >Specs</h2>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > ABS </h4>
+                            <h4  > {vehicleSpecs?.anti_brake_system} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > City Mileage </h4>
+                            <h4  > {vehicleSpecs?.city_mileage} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Drive Type </h4>
+                            <h4  > {vehicleSpecs?.drive_type} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Engine </h4>
+                            <h4  > {vehicleSpecs?.engine} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Fuel Type </h4>
+                            <h4  > {vehicleSpecs?.fuel_type} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Highway Mileage </h4>
+                            <h4  > {vehicleSpecs?.highway_mileage} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Made In </h4>
+                            <h4  > {vehicleSpecs?.made_in} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Make </h4>
+                            <h4  > {vehicleSpecs?.make} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Model </h4>
+                            <h4  > {vehicleSpecs?.model} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Steering Type </h4>
+                            <h4  > {vehicleSpecs?.steering_type} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Style </h4>
+                            <h4  > {vehicleSpecs?.style} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Transmission </h4>
+                            <h4  > {vehicleSpecs?.transmission} </h4>
+                          </div>
+                          <h2 className="text-primary" >Price</h2>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Price (above) </h4>
+                            <h4  > {vehiclePrice?.prices?.above} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Price (average) </h4>
+                            <h4  > {vehiclePrice?.prices?.average} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Price (below) </h4>
+                            <h4  > {vehiclePrice?.prices?.below} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Period (from) </h4>
+                            <h4  > {vehiclePrice?.period[0]} </h4>
+                          </div>
+                          <div className="d-flex  justify-content-between" >
+                            <h4  > Period (to) </h4>
+                            <h4  > {vehiclePrice?.period[1]} </h4>
+                          </div>
                         </Col>
                       </Row>
                     </>
